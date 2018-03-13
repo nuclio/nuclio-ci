dae763e17118932b705becf1ac87276cbfe238f7
ilaykav <ilay.kavitzky@gmail.com>
2018-03-13 19:05:44 +0200

    first review changes-2

--- github_status_updater/github_status_updater.py
+++ github_status_updater/github_status_updater.py
@@ -1,57 +1,25 @@
 import requests
 import json
+import os
 
 
 # event contains requset_body, which contains:
-#   status - state of commit to be updated
-#   event_info - information needed for updating
+#   state - state of commit to be updated
+#   commit_sha - sha of commit for updating state
+#   repo_url - commit's repository url
 def handler(context, event):
     request_body = json.loads(event.body)
     session = requests.Session()
 
-    # for updating github's status it is needed to be the repo's owner
-    session.auth = ('ilaykav', 'ilaykavtoken')
+    # get username & password from container's environment vars
+    repo_owner_username, repo_owner_token = os.environ.get('REPO_OWNER_USERNAME'),
+    os.environ.get('REPO_OWNER_OAUTH_TOKEN')
+    if not (repo_owner_username and repo_owner_token):
+        return 'Plesae configure environment vars REPO_OWNER_USERNAME, REPO_OWNER_OAUTH_TOKEN'
 
-    # get last commit's sha
-    last_commit_sha = get_last_commit_sha(request_body['event_info'])
+    session.auth = (repo_owner_username, repo_owner_token)
 
-    # resolve url from request_body, update status of commit to given status
+    # resolve url from request_body, update state of commit to given state
     # with github-api
-    url = request_body['event_info']['repository']['statuses_url']
-    session.post(url[:-5] + last_commit_sha,
-                 data='{\'state\': \'{}\'}'.format(request_body['status']))
-
-
-# get last commit of PR mentioned in the github response
-def get_last_commit_sha(request_body):
-    session = requests.Session()
-    session.auth = ('Nuci314', 'Nucitoken')
-    commits = 'commits'
-    next_commits = commits
-    num_page = 1
-    action_type = request_body['action']
-
-    # iterate over commits until reaching end of them (in case of github api
-    # sending multiple pages)
-    while True:
-
-        # handle cases where jsons are different
-        if action_type == 'created':
-            next_commits = session.get('{}/commits'.format(
-                request_body['issue']['pull_request']['url']),
-                params={'page': num_page}
-            )
-        elif action_type == 'synchronize':
-            next_commits = session.get(
-                '{}/commits'.format(request_body['pull_request']['url']),
-                params={'page': num_page})
-
-        # break when next page is empty
-        if not json.loads(next_commits.text):
-            break
-
-        commits = json.loads(next_commits.text)
-        num_page += 1
-
-    # get last commit's sha
-    return commits[-1]['sha']
+    session.post('{}/statuses/{}'.format(request_body['repo_url'], request_body['commit_sha']),
+                 json={'state': request_body['state']})
