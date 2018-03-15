6db60854397d0e6a7ea3943686e2480392e8f311
ilaykav <ilay.kavitzky@gmail.com>
2018-03-15 22:31:34 +0200

    fix database-insertion

--- database_init/database_init.py
+++ database_init/database_init.py
@@ -4,11 +4,12 @@ import json
 import parse
 
 
-# init database, gets github-slack usernames in event.body in format of githubname1_slackname1_githubnameN_slacknameN
+# init database, gets info to put in tanles in event.body in format of
+# fixtures: { table_name: [{col1_name: col1_value, col2_name: col2_value...}, {col1_name...}] table_name2:...}
 def handler(context, event):
 
     # get postgres connection information from container's environment vars
-    postgres_info = parse_info(os.environ.get('PGINFO'))
+    postgres_info = parse_env_var_info(os.environ.get('PGINFO'))
 
     # raise NameError if env var not found, or found in wrong format
     if postgres_info is None:
@@ -20,20 +21,12 @@ def handler(context, event):
     # connect to postgres database,
     conn = psycopg2.connect(host=postgres_host, user=postgres_user, password=postgres_password, port=postgres_port)
 
-    # get usernames from request to insert in USERS table
-    github_slack_usernames = json.loads(event.body)['users'].split('_')
-
-    # get every even and odd user together, to insert properly in users table
-    users = [(github_slack_usernames[i], github_slack_usernames[i + 1])
-             for i in range(0, len(github_slack_usernames), 2)]
-
     # proper commands to initialize database
     commands = ['create table test_cases (runnind_node oid, logs text, result int, job oid,'
                 ' artifact_test text) with oids',
                 'create table nodes (current_test_case oid) with oids',
                 'create table jobs (state int) with oids',
-                'create table users (github_username text, slack_username text) with oids',
-                'insert into users (github_username, slack_username) values {0}'.format(str(users)[1:-1])]
+                'create table users (github_username text, slack_username text) with oids']
 
     # cur is the cursor of current connection
     cur = conn.cursor()
@@ -42,13 +35,16 @@ def handler(context, event):
     for command in commands:
         cur.execute(command)
 
+    # process request - insert data accordingly
+    process_request(json.loads(event.body)['fixtures'], conn)
+
     # commit changes
     conn.commit()
 
 
 # gets string to process, return None if format is wrong, list of info if format is well writen -
 # return-list : username, password, host, port
-def parse_info(formatted_string):
+def parse_env_var_info(formatted_string):
     if formatted_string is not None:
 
         # check if default formatted string given
@@ -59,3 +55,20 @@ def parse_info(formatted_string):
         if parse.parse('{}:{}@{}', formatted_string) is not None:
             return list(parse.parse('{}:{}@{}', formatted_string)) + [5432]
     return None
+
+
+# gets request's info and connection, inserts data
+def process_request(request_json, database_connection):
+    cur = database_connection.cursor()
+
+    # insert data for every table in request
+    for table in request_json:
+        for row_info in request_json[table]:
+            cur.execute(get_add_query(table, row_info))
+
+
+# gets table name and row info (dict), returns insert query
+def get_add_query(table_name, row_info):
+    return 'insert into {0} {1} values {2}'.format(table_name,
+                                                   str(tuple(row_info.keys())).replace('\'', '').replace(',)', ')'),
+                                                   str(tuple(row_info.values())).replace(',)', ')'))
