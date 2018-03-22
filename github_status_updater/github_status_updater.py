2e325e085051b40c8a7b74c1fbcf9aed372d32fe
Ilay Kavitzky <ilay.kavitzky@gmail.com>
2018-03-22 15:44:16 +0200

    Add call function, duplicated across functions (#4)

--- github_status_updater/github_status_updater.py
+++ github_status_updater/github_status_updater.py
@@ -24,3 +24,17 @@ def handler(context, event):
     # with github-api
     session.post('{0}/statuses/{1}'.format(request_body['repo_url'], request_body['commit_sha']),
                  json={'state': request_body['state']})
+
+
+# calls given function with given arguments
+def call_function(function_name, function_arguments=None):
+    functions_ports = {
+        'database_init': 36543,
+        'github_status_updater': 36544,
+        'slack_notifier': 36545
+    }
+
+    # if given_host is specified post it instead of
+    given_host = os.environ.get('DOCKER_HOST', '172.17.0.1')
+    requests.post('http://{0}:{1}'.format(given_host, functions_ports[function_name]),
+                  data=function_arguments)
