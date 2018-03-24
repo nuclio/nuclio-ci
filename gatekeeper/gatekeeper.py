095201e03c1fffcf9fa8944f67d16eb06a8ba7ba
ilaykav <ilay.kavitzky@gmail.com>
2018-03-24 20:17:31 +0300

    review changes

--- gatekeeper/gatekeeper.py
+++ gatekeeper/gatekeeper.py
@@ -4,48 +4,57 @@ import parse
 import requests
 
 
-# get a webhook report in event.data, launch nuci if needed & permitted
+# get a webhook report in event.body, launch nuci if needed & permitted
 def handler(context, event):
 
-    # Load data from given json
+    # Load data from given json, init relevant variables
     webhook_report = json.loads(event.body)
     permission_sequence = 'Can one of the admins approve running tests for this PR?'
     launch_word = '@nuci approved'
-    whitelist = ['ilaykav']
+    session = invoke_github_authenticated_session()
+    pr = Pr(webhook_report)
 
     # check if event is not relevant don't do anything
-    if not is_event_relevant(webhook_report, launch_word, whitelist):
+    if not event_warrants_starting_integration_test(webhook_report):
         return
 
-    # check if pull request author is whitelisted -> start nuci
-    # if it's a comment -> it's a relevant comment (because of is_event_relevant()) -> start nuci
-    if webhook_report.get('comment') is None and webhook_report['pull_request']['user']['login'] not in whitelist:
+    # check if action allowed to run_integration_tests, start nuci if true
+    if not check_action_allowed(pr.author, 'run_integration_tests'):
 
-        # check if any admin permitted the PR, if so start nuci
-        if not admin_permitted(get_comments_url(webhook_report), launch_word, whitelist):
+        # check if anyone permitted the PR, start nuci if true
+        if not check_pr_whitelisted(pr, session, launch_word):
 
-            # add comment only if not added yet asking for admin to whitelist the PR
-            add_comment_if_doesnt_exist(webhook_report, permission_sequence)
+            # add comment only if not added yet asking for whitelister to whitelist the PR
+            pr.add_comment(permission_sequence, session, exclusive=True)
             return
 
     # TODO: call_function('run_job')
     context.logger.info('start nuci')
 
 
-# implement is_in_comments() with is_comment_white_listed() to check for presence of whitelister approval
-def admin_permitted(comments_url, launch_word, whitelist):
-    return is_in_comments(comments_url,
-                          launch_word,
-                          lambda comment_preferences: comment_preferences['user']['login'] in whitelist)
+# check if anyone from whitelist allowed run integration tests
+def check_pr_whitelisted(pr, session, launch_word):
+    comments = pr.get_comments(session, launch_word)
+
+    # if comments found with user that allowed to whitelist prs, return true
+    if len(comments) != 0:
+        for comment in comments:
+            if check_action_allowed(comment['user']['login'], 'whitelist_prs'):
+                return True
+
+    return False
 
 
-# add comment to issue's comments if it's not already there
-def add_comment_if_doesnt_exist(webhook_report, permission_sequence):
+# return if username allowed, according to given action & username
+def check_action_allowed(username, action):
+    run_integration_whitelist = ['ilaykav']
+    whitelist_prs = ['ilaykav']
 
-    # if permission sequence not in comments add one
-    if not is_in_comments(get_comments_url(webhook_report), permission_sequence):
-        session = get_github_authenticated_session()
-        session.post(get_comments_url(webhook_report), json={'body': permission_sequence})
+    # return right check-result according to given action & username
+    return {
+        'run_integration_tests': username in run_integration_whitelist,
+        'whitelist_prs': username in whitelist_prs
+    }[action]
 
 
 # checks if given str1 contains str2
@@ -53,39 +62,15 @@ def contains_ignore_case(str1, str2):
     return str2.lower() in str1.lower()
 
 
-# returns if it's issue comment & relevant - ignore other comments
-def is_event_relevant(webhook_report, launch_word, whitelist):
-
-    # If it's a comment, return true if the author is whitelisted and comment contains launch_word
-    comment = webhook_report.get('comment')
-    if comment is not None:
-        return comment['user']['login'] in whitelist and contains_ignore_case(comment['body'], launch_word)
-    return True
-
-
-# check for given word in any comment body, considers function(comment) if more preferences necessary
-def is_in_comments(comments_url, string_to_find, other_preference_function=None):
-    num_page = 0
-    session = requests.Session()
-    comments = json.loads(session.get(comments_url).text)
-
-    # iterate over comments until given word found or end of comments
-    while comments:
-        for comment in comments:
-
-            # check if comment's body contains string_to_find & if
-            # other_preference_function given check if it returns true
-            if contains_ignore_case(comment['body'], string_to_find) and \
-               other_preference_function(comment) if other_preference_function else True:
-                return True
-
-        num_page += 1
-        comments = json.loads(session.get(comments_url, params={'page': num_page}).text)
+# returns if the webhook event is relevant
+def event_warrants_starting_integration_test(webhook_report):
+    if webhook_report['action'] in ['opened', 'reopened', 'synchronize']:
+        return True
     return False
 
 
 # returns github authenticated session, using given environmental variable REPO_OWNER_DETAILS
-def get_github_authenticated_session():
+def invoke_github_authenticated_session():
 
     # env var REPO_OWNER_DETAILS should be in pattern username:access_token
     repo_owner_details = parse.parse('{}:{}', os.environ.get('REPO_OWNER_DETAILS'))
@@ -100,11 +85,49 @@ def get_github_authenticated_session():
     return session
 
 
-# return comments_url of the PR
-def get_comments_url(webhook_report):
-    action_type = webhook_report['action']
-    # handle cases where jsons are different
-    if action_type == 'created':
-        return webhook_report['issue']['pull_request']['comments_url']
+# pr object gives pr-related information and actions based on given webhook
+class Pr(object):
+    def __init__(self, webhook):
+        self._webhook = webhook
 
-    return webhook_report['pull_request']['comments_url']
+    # adds a comment with the given body. if exclusive is true, will first call
+    # get_comments to verify that it doesn't already exist
+    def add_comment(self, body, session, exclusive=False):
+        if exclusive:
+            if len(self.get_comments(session, body)) == 0:
+                session.post(self.get_comments_url(), json={'body': body})
+            return
+        session.post(self.get_comments_url(), json={'body': body})
+
+    # looks into the comments of the PR and returns the comments matching the body
+    def get_comments(self, session, body=None):
+        num_page = 1
+        comments = json.loads(session.get(self.get_comments_url()).text)
+        return_comments = []
+
+        # iterate over comments until given word found or end of comments
+        while len(comments) != 0:
+            for comment in comments:
+
+                # check if comment's body contains string_to_find & if
+                if body is None or contains_ignore_case(comment['body'], body):
+                    return_comments.append(comment)
+
+            num_page += 1
+            comments = json.loads(session.get(self.get_comments_url(), params={'page': num_page}).text)
+
+        return return_comments
+
+    # return comments_url of the PR
+    def get_comments_url(self):
+        action_type = self._webhook['action']
+        # handle cases where jsons are different
+        if action_type == 'created':
+            return self._webhook['issue']['pull_request']['comments_url']
+
+        return self._webhook['pull_request']['comments_url']
+
+    # resolve pr author from pr._webhook
+    @property
+    def author(self):
+        return self._webhook['pull_request']['user']['login']
