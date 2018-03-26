cd25dff1e680c0efb295c161fef83612d0580cad
ilaykav <ilay.kavitzky@gmail.com>
2018-03-26 10:55:57 +0300

    second review changes

--- gatekeeper/gatekeeper.py
+++ gatekeeper/gatekeeper.py
@@ -3,16 +3,17 @@ import json
 import parse
 import requests
 
+PERMISSION_SEQUENCE = 'Can one of the admins approve running tests for this PR?'
+LAUNCH_WORD = '@nuci approved'
+
 
 # get a webhook report in event.body, launch nuci if needed & permitted
 def handler(context, event):
 
     # Load data from given json, init relevant variables
     webhook_report = json.loads(event.body)
-    permission_sequence = 'Can one of the admins approve running tests for this PR?'
-    launch_word = '@nuci approved'
-    session = invoke_github_authenticated_session()
-    pr = Pr(webhook_report)
+    session = create_github_authenticated_session()
+    pr = Pr(webhook_report, session)
 
     # check if event is not relevant don't do anything
     if not event_warrants_starting_integration_test(webhook_report):
@@ -22,10 +23,10 @@ def handler(context, event):
     if not check_action_allowed(pr.author, 'run_integration_tests'):
 
         # check if anyone permitted the PR, start nuci if true
-        if not check_pr_whitelisted(pr, session, launch_word):
+        if not check_pr_whitelisted(pr, LAUNCH_WORD):
 
             # add comment only if not added yet asking for whitelister to whitelist the PR
-            pr.add_comment(permission_sequence, session, exclusive=True)
+            pr.add_comment(PERMISSION_SEQUENCE, exclusive=True)
             return
 
     # TODO: call_function('run_job')
@@ -33,28 +34,25 @@ def handler(context, event):
 
 
 # check if anyone from whitelist allowed run integration tests
-def check_pr_whitelisted(pr, session, launch_word):
-    comments = pr.get_comments(session, launch_word)
+def check_pr_whitelisted(pr, launch_word):
+    comments = pr.get_comments(launch_word)
 
     # if comments found with user that allowed to whitelist prs, return true
-    if len(comments) != 0:
-        for comment in comments:
-            if check_action_allowed(comment['user']['login'], 'whitelist_prs'):
-                return True
+    for comment in comments:
+        if check_action_allowed(comment['user']['login'], 'whitelist_prs'):
+            return True
 
     return False
 
 
 # return if username allowed, according to given action & username
 def check_action_allowed(username, action):
-    run_integration_whitelist = ['ilaykav']
-    whitelist_prs = ['ilaykav']
+    actions_by_user = {
+        'ilaykav': ['run_integration_tests', 'whitelist_prs']
+    }
 
-    # return right check-result according to given action & username
-    return {
-        'run_integration_tests': username in run_integration_whitelist,
-        'whitelist_prs': username in whitelist_prs
-    }[action]
+    # return return true if action in user's optional actions
+    return action in actions_by_user.get(username, [])
 
 
 # checks if given str1 contains str2
@@ -64,13 +62,11 @@ def contains_ignore_case(str1, str2):
 
 # returns if the webhook event is relevant
 def event_warrants_starting_integration_test(webhook_report):
-    if webhook_report['action'] in ['opened', 'reopened', 'synchronize']:
-        return True
-    return False
+    return webhook_report['action'] in ['opened', 'reopened', 'synchronize']
 
 
 # returns github authenticated session, using given environmental variable REPO_OWNER_DETAILS
-def invoke_github_authenticated_session():
+def create_github_authenticated_session():
 
     # env var REPO_OWNER_DETAILS should be in pattern username:access_token
     repo_owner_details = parse.parse('{}:{}', os.environ.get('REPO_OWNER_DETAILS'))
@@ -87,22 +83,23 @@ def invoke_github_authenticated_session():
 
 # pr object gives pr-related information and actions based on given webhook
 class Pr(object):
-    def __init__(self, webhook):
+    def __init__(self, webhook, session):
         self._webhook = webhook
+        self._session = session
 
     # adds a comment with the given body. if exclusive is true, will first call
     # get_comments to verify that it doesn't already exist
-    def add_comment(self, body, session, exclusive=False):
+    def add_comment(self, body, exclusive=False):
         if exclusive:
-            if len(self.get_comments(session, body)) == 0:
-                session.post(self.get_comments_url(), json={'body': body})
+            if len(self.get_comments(body)) == 0:
+                self._session.post(self._get_comments_url(), json={'body': body})
             return
-        session.post(self.get_comments_url(), json={'body': body})
+            self._session.post(self._get_comments_url(), json={'body': body})
 
     # looks into the comments of the PR and returns the comments matching the body
-    def get_comments(self, session, body=None):
+    def get_comments(self, body=None):
         num_page = 1
-        comments = json.loads(session.get(self.get_comments_url()).text)
+        comments = json.loads(self._session.get(self._get_comments_url()).text)
         return_comments = []
 
         # iterate over comments until given word found or end of comments
@@ -114,20 +111,20 @@ class Pr(object):
                     return_comments.append(comment)
 
             num_page += 1
-            comments = json.loads(session.get(self.get_comments_url(), params={'page': num_page}).text)
+            comments = json.loads(self._session.get(self._get_comments_url(), params={'page': num_page}).text)
 
         return return_comments
 
+    # resolve pr author from pr._webhook
+    @property
+    def author(self):
+        return self._webhook['pull_request']['user']['login']
+
     # return comments_url of the PR
-    def get_comments_url(self):
+    def _get_comments_url(self):
         action_type = self._webhook['action']
         # handle cases where jsons are different
         if action_type == 'created':
             return self._webhook['issue']['pull_request']['comments_url']
 
         return self._webhook['pull_request']['comments_url']
-
-    # resolve pr author from pr._webhook
-    @property
-    def author(self):
-        return self._webhook['pull_request']['user']['login']
